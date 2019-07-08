767293fcdfaf3e2920658d9968ea4a02b3ca6fd8
Legendsmith <legendsmith.au@gmail.com>
2019-07-09 02:16:12 +1000

    Initial commit

---
+++ SolidIceWall/Class1.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using UnityEngine;
+using RoR2;
+
+
+namespace SolidIceWall
+{
+    public class SolidIceWallLoader
+
+    {
+        public void Awake()
+        {
+            Chat.AddMessage("Loaded SolidIcewall!");
+            GameObject pillarprefab = Resources.Load<GameObject>("prefabs/projectiles/mageicewallpillarprojectile");
+            pillarprefab.layer = 11;
+        }
+    }
+}
---
+++ ThunderDownUnder/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ThunderDownUnder
+{
+    public class Class1
+    {
+    }
+}
